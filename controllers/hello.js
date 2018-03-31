c411885cd49809ba1f685f732be6c4f7ce4b7a75
假正经哥哥 <xuanye.wan@gmail.com>
2018-03-31 22:16:01 +0800

    hello controller 更新为正式世界的异步调用方式

--- controllers/hello.js
+++ controllers/hello.js
@@ -1,1 +1,15 @@
-module.exports = (req)=>`Hello ${req.params.who}`;
+const { send } = require('micro');
+
+
+module.exports = async (req,res)=> {
+    let hello = await getHello(req.params.who);
+    send(res,200,hello);
+}
+
+
+
+function getHello(name){
+    return new Promise((resolve,reject)=>{
+        resolve("Hello "+ name +"!");
+    });
+}
